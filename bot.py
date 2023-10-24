f21e33667c6a5991397e078aa6357a4c6c7e03bf
takeoff37808 <64719447+Takeoff0518@users.noreply.github.com>
2023-10-24 17:02:23 +0800

    add: Init nb2

---
+++ bot.py
@@ -0,0 +1,12 @@
+import nonebot
+from nonebot.adapters.onebot.v11 import Adapter
+
+nonebot.init()
+
+driver = nonebot.get_driver()
+driver.register_adapter(Adapter)
+
+nonebot.load_from_toml("pyproject.toml")
+
+if __name__ == "__main__":
+    nonebot.run()
